df11a282265c08cbe3ed610dd0ba8f244651eb95
kevinmcalear <kevin.mcalear@gmail.com>
2014-07-22 15:50:17 -0400

    Revert "building out api"
    This reverts commit 88291332d780a0ab7b63ff95e77307437eda559d.

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  validates :email, presence: :true, uniqueness: :true 
-  validates :password_digest, presence: :true
-  validates :user_name, presence: :true, uniqueness: :true
-
-  has_secure_password
-end
